0410d7ea11e095941cf2838b346a094228dd4157
Satyajit-Pawar <satyajitpawar2003@gmail.com>
2025-11-18 19:29:52 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello! This is a simple Go web server.\n")
+		fmt.Fprintf(w, "Hello! This is a simple Go web server form satyajit.\n")
 		fmt.Fprintf(w, "This is a change version of this file.\n")
 	})
 
